cf14a5894842c3248d5f1d972b7c9df8a6552135
副島 侑汰朗 <yutarosoejima54@gmail.com>
2016-05-27 11:49:09 +0900

    Prepare endpoint tentatively

--- server.go
+++ server.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+)
+
+type (
+	result struct {
+		Query string
+	}
+)
+
+func search(c echo.Context) error {
+	q := c.QueryParam("q")
+	return c.JSON(http.StatusOK, result{ Query: q })
+}
 
 func main() {
-    m := martini.Classic()
-    m.Get("/", func() string {
-        return "Hello world!"
-    })
-    m.Run()
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/search", search)
+
+	e.Run(standard.New(":3000"))
 }
