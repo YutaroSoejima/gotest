32d054cbd1e8698cf6d03541909f75536d217acd
Yosuke Nakamura <nakamura_yosuke_xa@cyberagent.co.jp>
2016-05-29 01:12:39 +0900

    Add: func|getTopics(return topics for query via MongoDB)

--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"reflect"
+	"strings"
 	"unicode/utf8"
 
+	"github.com/franela/goreq"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
@@ -38,9 +41,40 @@ type (
 		WholeText string
 		PageRank  float64
 	}
+
+	// .../prod/classify?word={QueryParameter.Word}
+	QueryParameter struct {
+		Word string
+	}
 )
 
+// return topics for query(::query_topics)
+func getTopics(query string) map[string]string {
+	word := QueryParameter{Word: query}
+	response, _ := goreq.Request{
+		Uri:         "https://jpdtd1hnzf.execute-api.ap-northeast-1.amazonaws.com/prod/classify",
+		QueryString: word,
+	}.Do()
+
+	body, _ := response.Body.ToString()
+	body = strings.Trim(body, "{}")
+	topics := strings.Split(body, ",")
+	query_topics := make(map[string]string)
+	for _, topic := range topics {
+		elm := strings.Split(topic, ":")
+		elm[0] = strings.Trim(elm[0], " ")
+		query_topics[elm[0]] = elm[1]
+	}
+
+	return query_topics
+}
+
 func search(c echo.Context) error {
+	query_param := c.QueryParam("query")
+	query_topics := getTopics(query_param)
+	fmt.Println(query_topics)
+	// [REMIND] when requesting to elastic, also use query_topics
+
 	meta := metaInfo{c.QueryParam("query"), 0.34, 12345, 1}
 	var data []resultItem
 	client, _ := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL("http://52.68.230.203:9200/"))
